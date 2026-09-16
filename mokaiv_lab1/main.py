1686cd2254be993750a6c7c3a02611e12c0d632f
LazySloth322 <example.com>
2026-09-16 15:16:50 +0300

    email check added

--- mokaiv_lab1/main.py
+++ mokaiv_lab1/main.py
@@ -10,27 +10,48 @@ import json
 
 def getFileNames(dirPath):
     fileNames = os.listdir(dirPath)
+    for i in range(len(fileNames)):
+        fileNames[i]=dirPath+fileNames[i]
     print(fileNames)
 
     return fileNames
 
+def getInfo(fileName, mode=0): #0=base; 1=check
+    with open(fileName, "r", encoding="utf-8") as file:
+        data = json.load(file)
+
+        if mode:
+            reference = ["id","datetime","sender","subject","attachment","text"]
+            dataKeys = list(data.keys())
+            #print(dataKeys)
+            try:
+                for i in range(len(reference)):
+                    if(dataKeys[i]!=reference[i]): return 1
+            except Exception as e:
+                print(e)
+                return 1
+            return 0
+        elif not mode:
+            pass
+
 def validateEmail(fileNames):
+    validList = []
     for file in fileNames:
         fileExtension=file.split(".")
         if fileExtension[len(fileExtension)-1]=="json":
-            print(file)
-
-
-        else:
-            pass
-
+            if getInfo(file,mode=1):
+                print(file+" is invalid")
+            else:
+                validList.append(file)
+    #print(validList)
+    return validList
 
 
 def main(dirPath):
     fileNames = getFileNames(dirPath)
-    validateEmail(fileNames)
+    validMailList = validateEmail(fileNames)
 
-    pass
+    print(validMailList)
 
 
 if __name__ == "__main__":
