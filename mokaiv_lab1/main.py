3648d1ab7f8c783121f9408bdd665f5784b17dc6
LazySloth322 <example.com>
2026-09-24 13:29:38 +0300

    added unzip method

--- mokaiv_lab1/main.py
+++ mokaiv_lab1/main.py
@@ -1,13 +1,27 @@
 # TODO
 # 1. check if email is valid (contain all necessary fields)
-# 2. check spf
-# 3. check dkim
-# 4. mark as phishing (dmarc)
-# 5. return amount of phishing emails
+# 2. check spf (not working)
+# 3. mark as phishing (suspicion coef: 0-valid, 0<...<2-sus, >2 phishing)
+#   3.1 no link in mail +=0
+#   3.2 https link +=1
+#   3.3 http link +=2
+#   3.4 sender domain doesnt equal link in mail +=1
+#   3.5 key words? (ограничен, блокировке/заблокирован, заморожен, восстановить, отменен, ответить) +=1
+#   3.6 there is an attachment +=1
+# 4. return amount of phishing emails
 
 import os
 import json
 import dns.resolver
+import datetime
+import zipfile
+
+def unzip(archivePath):
+    extractionPath = f"./{str(datetime.datetime.now().timestamp()).replace('.', '-')}/"
+    with zipfile.ZipFile(archivePath, 'r') as zipRef:
+        zipRef.extractall(extractionPath)
+
+    return extractionPath+"phish/"
 
 def getFileNames(dirPath):
     fileNames = os.listdir(dirPath)
@@ -32,7 +46,9 @@ def getInfo(fileName, mode=0): #0=base; 1=check
                 return 1
             return 0
         elif not mode:
-            pass
+            data = json.load(file)
+            return [data["sender"],data["text"], data["attachment"]]
+
 
 def validateEmail(fileNames):
     validList = []
@@ -53,25 +69,72 @@ def getSPF(domain):
             return txt
     return None
 
+def verifyEmail(validMailList):
+    result={"clean":[],"sus":[],"phishing":[]}
+    susWords = ["ограничен","блокиров","заморожен","восстановит","отменен","ответить","ответьте"]
+    for mail in validMailList:
+        score = 0
+        info = getInfo(mail)
 
-def main(dirPath):
-    fileNames = getFileNames(dirPath)
-    totalAmount = len(fileNames)
+        containsURL = False
 
-    validMailList = validateEmail(fileNames)
-    validAmount = len(validMailList)
+        if ("http" in info[1]):
+            containsURL=True
+            if ("https" in info[1]):
+                score+=1
+            else:
+                score+=2
 
-    print(validMailList)
+        if containsURL:
+            senderURL = info[0].split("@")[1]
+            if not (senderURL in info[1]):
+                score+=1
 
-    for i in range(validAmount):
-        with open(validMailList[i], "r", encoding="utf-8") as file:
-            data = json.load(file)
-            try:
-                print(getSPF(data["sender"].split("@")[1]))
-            except Exception as e:
-                print(e)
+        for word in susWords:
+            if (word in info[1]):
+                score+=1
+                break
+
+        if info[2]:
+            score+=1
+
+        if score==0:
+            result["clean"].append(mail)
+        elif (score>0 and score<2):
+            result["sus"].append(mail)
+        elif (score>2):
+            result["phishing"].append(mail)
+    return result
+
+def main():
+    print("Path to .zip file:")
+    archivePath = str(input())
+    print("")
+
+    try:
+        dirPath = unzip(archivePath)
+        #print(dirPath)
+        fileNames = getFileNames(dirPath)
+        totalAmount = len(fileNames)
+
+        validMailList = validateEmail(fileNames)
+        validAmount = len(validMailList)
+
+        result = verifyEmail(validMailList)
+
+    except Exception as e:
+        print(f"Error: {e}")
+        return 1
+
+    print("\nValid emails: ", validAmount, " (total ",totalAmount,")")
+    print("Clean emails: ",len(result["clean"]))
+    print("Suspicious emails:",len(result["sus"]))
+    print("Phishing emails: ",len(result["phishing"]))
+
+    #print(result["clean"])
+    # print(result["sus"])
+    # print(result["phishing"])
 
 
 if __name__ == "__main__":
-    dirPath = "./test/"
-    main(dirPath)
+    main()
